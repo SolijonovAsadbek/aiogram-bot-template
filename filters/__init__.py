96f5c8b4bfbc8b907db22cdb394b50acb8521d53
Asadbek <84005743+SolijonovAsadbek@users.noreply.github.com>
2021-08-21 13:17:52 +0300

    Add files via upload

--- filters/__init__.py
+++ filters/__init__.py
@@ -1,9 +1,11 @@
-from aiogram import Dispatcher
-
 from loader import dp
-# from .is_admin import AdminFilter
+from .admins import AdminFilter
+from .group_filter import GroupFilter
+from .private_filter import Private
+
 
 
 if __name__ == "filters":
-    #dp.filters_factory.bind(is_admin)
-    pass
+    dp.filters_factory.bind(AdminFilter)
+    dp.filters_factory.bind(Private)
+    dp.filters_factory.bind(GroupFilter)
